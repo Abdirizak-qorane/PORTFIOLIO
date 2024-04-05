cdc4d6dd4625701e4751b91981971cb999c73ba5
Abdirizak Qorane <crisaaqqorane@gmail.com>
2024-04-05 16:07:59 +0300

    first commit

---
+++ src/Componenet/Infor-section/Section-two.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function InfoSection() {
+  return (
+    <div className="info">
+      <span className="left-cursor">
+      
+      </span>
+      <div className="target">
+        <h1>Hi, I'm Designer A.rizak Ismail</h1>
+      </div>
+      <span className="right-cursor">
+        
+      </span>
+    </div>
+  );
+}
+
+export default InfoSection;
